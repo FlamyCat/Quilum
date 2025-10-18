83bf651491a0b81623f2a122a13591793260e657
Александр Сергиенко <s_sandr06@mail.ru>
2025-10-18 23:11:11 +0300

    Добавлен ColorPicker

---
+++ src/lib/components/ColorPicker.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    let { color = $bindable<string>("#000") } = $props();
+    let colorPicker = $state<HTMLElement>();
+
+    $effect(() => {
+        colorPicker!.style.backgroundColor = color!;
+    })
+</script>
+
+<div bind:this={colorPicker} class="size-10 rounded-lg">
+    <input placeholder="{color}" type="color" bind:value={color} class="opacity-0 size-full">
+</div>
