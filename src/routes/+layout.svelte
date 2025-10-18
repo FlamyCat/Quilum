c0faddc81aa3ac09806f4c16c3e6b278a900ed38
Александр Сергиенко <s_sandr06@mail.ru>
2025-10-18 20:41:50 +0300

    Общая разметка интерфейса приложения

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import "../app.css";
+    import Page from "./Body.svelte";
+    import Navbar from "./Navbar.svelte";
+
+    let { children } = $props();
+</script>
+
+<div class="flex gap-4 p-4 h-full">
+    <Page>
+        {@render children()}
+    </Page>
+
+    <Navbar />
+</div>
---
+++ src/routes/Navbar.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    let { children = () => null } = $props();
+</script>
+
+<div class="w-[min(10vw,90px)] p-2 flex items-center">
+    {@render children()}
+</div>
