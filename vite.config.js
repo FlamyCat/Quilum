3267be0ceb307cba1104fb4dee3804eaa5fdda90
Александр Сергиенко <s_sandr06@mail.ru>
2026-04-22 22:28:14 +0300

    Revert "Исправлена ошибка, при которой приложение могло собираться и запускаться"
    This reverts commit 0a82292410eb7e72bfb7d73875cd13c0c103b9ac.

--- vite.config.js
+++ vite.config.js
@@ -8,8 +8,8 @@ const host = process.env.TAURI_DEV_HOST;
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
     plugins: [
-        await sveltekit(),
         tailwindcss(),
+        await sveltekit(),
     ],
 
     // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
