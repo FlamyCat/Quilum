f538a3d45ca732dba810b52392bd096be9016ba4
Александр Сергиенко <s_sandr06@mail.ru>
2026-04-26 15:08:35 +0300

    Разделение Slot на структуру модели и запись для реализации графовых связей

--- src-tauri/crates/quilum/src/model/slot.rs
+++ src-tauri/crates/quilum/src/model/slot.rs
@@ -1,14 +1,21 @@
+use super::task::Task;
 use chrono::{NaiveDateTime, TimeDelta};
+use surrealdb::types::RecordId;
 
 #[derive(Clone, Debug)]
 pub(crate) struct Slot {
     starts_at: NaiveDateTime,
     ends_at: NaiveDateTime,
+    tasks: Vec<Task>,
 }
 
 impl Slot {
     pub fn new(starts_at: NaiveDateTime, ends_at: NaiveDateTime) -> Self {
-        Self { starts_at, ends_at }
+        Self {
+            starts_at,
+            ends_at,
+            tasks: Vec::new(),
+        }
     }
 
     pub fn starts_at(&self) -> NaiveDateTime {
@@ -22,4 +29,11 @@ impl Slot {
     pub fn duration(&self) -> TimeDelta {
         self.ends_at - self.starts_at
     }
+}
+
+#[derive(Clone, Debug)]
+struct SlotRecord {
+    id: RecordId,
+    starts_at: NaiveDateTime,
+    ends_at: NaiveDateTime,
 }
